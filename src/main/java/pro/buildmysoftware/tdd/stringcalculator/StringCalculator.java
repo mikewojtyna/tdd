ac4e085ef99434b5fe4ce32454aa2823377aaac9
Mike Wojtyna <mike@buildmysoftware.pro>
2018-09-13 00:42:31 +0200

    Add string calculator step 4 - refactor

--- src/main/java/pro/buildmysoftware/tdd/stringcalculator/StringCalculator.java
+++ src/main/java/pro/buildmysoftware/tdd/stringcalculator/StringCalculator.java
@@ -7,14 +7,7 @@ class StringCalculator {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
-		String[] numbersArray = numbers.split(",");
-		if (numbersArray.length > 1) {
-			int sum = 0;
-			for (String number : numbersArray) {
-				sum += Integer.valueOf(number);
-			}
-			return sum;
-		}
-		return Integer.valueOf(numbers);
+		return Stream.of(numbers.split(",")).mapToInt
+			(Integer::valueOf).sum();
 	}
 }
