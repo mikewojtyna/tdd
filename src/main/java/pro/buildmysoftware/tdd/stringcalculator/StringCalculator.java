395baabdb2580d0931d833595257eb9662d787d9
Mike Wojtyna <mike@buildmysoftware.pro>
2018-09-13 01:30:14 +0200

    Add string calculator step 6 - refactor

--- src/main/java/pro/buildmysoftware/tdd/stringcalculator/StringCalculator.java
+++ src/main/java/pro/buildmysoftware/tdd/stringcalculator/StringCalculator.java
@@ -3,21 +3,28 @@ package pro.buildmysoftware.tdd.stringcalculator;
 import java.util.stream.Stream;
 
 class StringCalculator {
+	private static final String NUMBERS_DELIMITER = ",";
+
 	static int add(String numbers) {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
-		return Stream.of(normalizeString(numbers).split(",")).mapToInt
-			(Integer::valueOf).sum();
+		return Stream.of(normalizeString(numbers).split
+			(NUMBERS_DELIMITER)).mapToInt(Integer::valueOf).sum();
 	}
 
 	private static String normalizeString(String numbers) {
-		boolean startsWithDelimiter = numbers.startsWith("//;\n");
-		if (startsWithDelimiter) {
-			char delimiter = numbers.charAt(2);
+		String extractedNumbers = extractNumbers(numbers);
+		return extractedNumbers.replace("\n", NUMBERS_DELIMITER);
+	}
+
+	private static String extractNumbers(String numbers) {
+		if (numbers.startsWith("//;\n")) {
+			String delimiter = String.valueOf(numbers.charAt(2));
 			String numbersAfterDelimiter = numbers.substring(4);
-			return numbersAfterDelimiter.replace(delimiter, ',');
+			return numbersAfterDelimiter.replace(delimiter,
+				NUMBERS_DELIMITER);
 		}
-		return numbers.replace('\n', ',');
+		return numbers;
 	}
 }
