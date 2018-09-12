e6d9bf8fdde056bf64129078ef4b2e17892827fb
Mike Wojtyna <mike@buildmysoftware.pro>
2018-09-12 23:32:25 +0200

    Add string calculator step 0 - refactor

--- src/test/java/pro/buildmysoftware/tdd/stringcalculator/StringCalculatorTest.java
+++ src/test/java/pro/buildmysoftware/tdd/stringcalculator/StringCalculatorTest.java
@@ -1,22 +1,22 @@
 package pro.buildmysoftware.tdd.stringcalculator;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Fail.fail;
 
 class StringCalculatorTest {
-	@DisplayName("should return 0 when input is empty")
-	@Test
-	void test0() throws Exception {
-		// given
-		String numbers = "";
-
+	@DisplayName("should calculate sum according to the specification")
+	@ParameterizedTest(name = "given numbers: \"{0}\", expected sum: {1}")
+	@CsvSource({
+		// empty string
+		"'', 0"})
+	void test(String numbers, int expectedSum) throws Exception {
 		// when
 		int sum = StringCalculator.add(numbers);
 
 		// then
-		assertThat(sum).isEqualTo(0);
+		assertThat(sum).isEqualTo(expectedSum);
 	}
 }
