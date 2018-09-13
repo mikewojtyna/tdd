a71981e8562a3a046585b20980bba2c782568cc4
Mike Wojtyna <mike@buildmysoftware.pro>
2018-09-13 02:24:13 +0200

    Add string calculator step 8 - refactor

--- src/main/java/pro/buildmysoftware/tdd/stringcalculator/StringCalculator.java
+++ src/main/java/pro/buildmysoftware/tdd/stringcalculator/StringCalculator.java
@@ -1,6 +1,8 @@
 package pro.buildmysoftware.tdd.stringcalculator;
 
+import java.util.Collection;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 class StringCalculator {
@@ -10,30 +12,29 @@ class StringCalculator {
 		if (numbers.isEmpty()) {
 			return 0;
 		}
-		String[] parsedNumbers = normalizeString(numbers).split
-			(NUMBERS_DELIMITER);
-		Optional<Integer> negativeNumber = findNegativeNumber
-			(parsedNumbers);
+		Collection<Integer> parsedNumbers = parseNumbers(numbers);
+		validateNonNegativeNumbers(parsedNumbers);
+		return parsedNumbers.stream().reduce(0, Integer::sum);
+	}
+
+	private static void validateNonNegativeNumbers(Collection<Integer>
+							       parsedNumbers) {
+		Optional<Integer> negativeNumber = parsedNumbers.stream()
+			.filter(n -> n < 0).findFirst();
 		negativeNumber.ifPresent(number -> {
 			throw new IllegalArgumentException("negatives not " +
 				"allowed: " + number);
 		});
-		return Stream.of(parsedNumbers).mapToInt(Integer::valueOf)
-			.sum();
 	}
 
-	private static Optional<Integer> findNegativeNumber(String[]
-								    parsedNumbers) {
-		return Stream.of(parsedNumbers).map(Integer::valueOf).filter(n
-			-> n < 0).findFirst();
+	private static Collection<Integer> parseNumbers(String numbers) {
+		String extractedNumbers = extractNumbersAfterDelimiter
+			(numbers).replace("\n", NUMBERS_DELIMITER);
+		return Stream.of(extractedNumbers.split(NUMBERS_DELIMITER))
+			.map(Integer::valueOf).collect(Collectors.toList());
 	}
 
-	private static String normalizeString(String numbers) {
-		String extractedNumbers = extractNumbers(numbers);
-		return extractedNumbers.replace("\n", NUMBERS_DELIMITER);
-	}
-
-	private static String extractNumbers(String numbers) {
+	private static String extractNumbersAfterDelimiter(String numbers) {
 		if (numbers.matches("^//.\n.*$")) {
 			String delimiter = String.valueOf(numbers.charAt(2));
 			String numbersAfterDelimiter = numbers.substring(4);
